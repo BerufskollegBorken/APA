c1572d5e15c156e40cff0f3eea07d85f64530f5d
Stefan Bäumer <stbaeumer@users.noreply.github.com>
2020-04-07 13:32:02 +0200

    ...

---
+++ APA/StudentgroupStudent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Globalization;
+
+namespace APA
+{
+    public class StudentgroupStudent
+    {
+        public int StudentId { get; private set; }
+        public string Forename { get; private set; }
+        public string Name { get; private set; }
+        public object Studentgroup { get; private set; }
+        public string Subject { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+
+        public StudentgroupStudent(string line)
+        {
+            var x = line.Split('\t');
+            StudentId = Convert.ToInt32(x[0]);
+            Name = x[1];
+            Forename = x[2];
+            Studentgroup = x[3];
+            Subject = x[4];
+            StartDate = GetDatum(x[5]);
+            EndDate = GetDatum(x[6]);
+        }
+                
+        private DateTime GetDatum(string datumString)
+        {
+            if (datumString == "")
+            {
+                return new DateTime();
+            }
+            return DateTime.ParseExact(datumString, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ APA/StudentgroupStudents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace APA
+{
+    public class StudentgroupStudents : List<StudentgroupStudent>
+    {
+        public StudentgroupStudents()
+        {
+            using (StreamReader reader = new StreamReader(Global.InputStudentgroupStudentsCsv))
+            {
+                string überschrift = reader.ReadLine();
+
+                while (true)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line != null)
+                    {
+                        StudentgroupStudent studentgroupStudent = new StudentgroupStudent(line);
+                        this.Add(studentgroupStudent);
+                    }
+
+                    if (line == null)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine(("StudentgroupStudents " + ".".PadRight(this.Count / 150, '.')).PadRight(48, '.') + (" " + this.Count).ToString().PadLeft(4), '.');
+            }
+        }
+    }
+}
